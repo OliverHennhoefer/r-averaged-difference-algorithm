ec92b82bc02280a762656fd851b1aaf14f76b765
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2020-10-25 14:38:17 +0100

    Added Possibility for Inverse Distance Weighting
    Mode as new parameter to choose between "linear" weights and "inverse distance" weights.

--- avgDiff.R
+++ avgDiff.R
@@ -1,55 +1,38 @@
-avgDiff <- function(data, attribute.col, k) {
+avgDiff <- function(data, attribute.col, k, ranks=1, power = 1, mode="linear") {
   require(data.table)
   require(FNN)
   require(sp)
   
-  #KNN data.table
   knn <- get.knn(sp::coordinates(data), k=k)
   near.neighbors <- as.data.table(knn)
   
-  #knearest <- knn2nb(knearneigh(coords, k = nn))
-  #k.distances <- nbdists(knearest, coords)
-  #invd2a <- lapply(k.distances, function(x) (1/(x/100)))
-  #weig. <- nb2listw(knearest,glist = invd2a,style = "B")
+  if (mode == "linear") {
+    near.neighbors <- near.neighbors[,(k+1):ncol(near.neighbors)]
+    near.neighbors$sum <- rowSums(near.neighbors, na.rm = FALSE, dims = 1)
+    weights <- near.neighbors[,1:(ncol(near.neighbors)-1)]/near.neighbors$sum
+  }
   
-  #Extract Distances without Indices (introduced by as.data.table)
-  near.neighbors <- near.neighbors[,(k+1):ncol(near.neighbors)]
+  if (mode == "inverse") {
+    near.neighbors <- near.neighbors[,(k+1):ncol(near.neighbors)]
+    weights <-(1/(near.neighbors^power))
+  }
   
-  #Sum Distances for Spatial Weighting
-  near.neighbors$sum <- rowSums(near.neighbors, na.rm = FALSE, dims = 1)
-  
-  #Divide every Distance by Sum to get the Weight
-  weights <- near.neighbors[,1:(ncol(near.neighbors)-1)]/near.neighbors$sum
-  
-  #Replace Indeces by corresponding Attribute
   col <- which(colnames(data@data)==attribute.col)
   attributes <- data@data[c(knn$nn.index), col]
   attributes <- data.frame(matrix(unlist(attributes), ncol=k), stringsAsFactors=FALSE)
-  #identical(dim(weights), dim(attributes)) #TRUE!
   
-  #Add Index of Point that corresponds to Nearest Neighbors Attributes
   table <- cbind(seq(1:nrow(attributes)), attributes)
   
-  #Calculate Absolute Difference 
   difference <- abs(data@data[c(table[,1]), col] - table[,2:ncol(table)])
   
-  #Calculate Weighted Difference
   weighted_difference <- as.data.table(difference) * weights
   
-  #Bind Averaged Difference with corresponding Index (Decreasing Order)
   result <- as.data.table(cbind(seq(1:nrow(weighted_difference)), as.data.table(rowSums(weighted_difference))))
   names(result) <- c("ID", "AvgDiff")
   result <- result[order(-AvgDiff)]
-  result
   
+  data$id <- 1:nrow(data)
+  ranked_diffs <- result[1:2000,1]
+  data <- subset(data, !data$Id %in% as.list(ranked_diffs$ID))
+  data
 }
-
-##Apply Function
-#averaged_differences <- avgDiff(data, attribute.col, k=355)
-
-##Select top 1200 Outliers (Example)
-#ranked_differences <- averaged_differences[1:1200,1]
-
-##Subset Original Data Set
-#cleansed <- subset(data, !data$id %in% as.list(ranked_differences$ID))
-#plot(cleansed)
